b7b9e4eb3b7222f883daa7d61d0a637caa403a59
sakidpilot <105039350+sakidpilot@users.noreply.github.com>
2024-11-11 18:49:08 +0200

    Update limitRequest.middleware.js

--- backend/middleware/limitRequest.middleware.js
+++ backend/middleware/limitRequest.middleware.js
@@ -3,6 +3,6 @@ import rateLimit from "express-rate-limit";
 // Rate limiter for login route
 export const limitRequest = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 1000, // Limit each IP to 4 requests per windowMs //change to 5
+    max: 5, // Limit each IP to 4 requests per windowMs //change to 5
     message: 'Too many login attempts from this IP, please try again after 15 minutes.',
 });
